00c4cce81f48ef65e05d84957e872742195566a1
Aditya Eka Pranata <aepranata@gmail.com>
2023-05-01 20:45:09 +0700

    rosy: Add some SHRP information
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- twrp_rosy.mk
+++ twrp_rosy.mk
@@ -20,3 +20,10 @@ PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5
+
+# Inherit some common SHRP stuff
+SHRP_DEVICE_CODE := rosy
+SHRP_PATH := device/xiaomi/$(SHRP_DEVICE_CODE)
+SHRP_MAINTAINER := aepranata
+SHRP_REC_TYPE := Normal
+SHRP_DEVICE_TYPE := A_Only
