3fc0451e9d1df6196cc4af83addbfaf401ad5dac
Aditya Eka Pranata <aepranata@gmail.com>
2023-05-01 10:06:44 +0700

    rosy: Adapt for Android 12.1
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -5,4 +5,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/xiaomi/rosy
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_PLATFORM := msm8953
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# QCOM standard decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt
