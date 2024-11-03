885d546d355b1f021e9126f3e9d895f6a53f4044
Aditya Eka Pranata <aepranata@gmail.com>
2024-11-03 13:33:42 +0700

    rosy: Adapt for Android-10
    Change-Id: I7c0f49d649a3684289b0b146da61b3f59284a810
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,2 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_rosy.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_rosy-user \
-    omni_rosy-userdebug \
-    omni_rosy-eng
